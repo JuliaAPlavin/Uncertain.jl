93f7838b9782b111ef7a95c3f43888d760b8a61b
Alexander Plavin <alexander@plav.in>
2025-01-01 00:00:00 +0000

    support any MCM.AbstractParticles

--- ext/MonteCarloMeasurementsExt.jl
+++ ext/MonteCarloMeasurementsExt.jl
@@ -3,8 +3,8 @@ module MonteCarloMeasurementsExt
 import MonteCarloMeasurements as MCM
 using Uncertain
 
-(::Type{T})(x::MCM.Particles) where {T<:U.Value} = T(MCM.pmean(x), MCM.pstd(x))
+(::Type{T})(x::MCM.AbstractParticles) where {T<:U.Value} = T(MCM.pmean(x), MCM.pstd(x))
 
-MCM.Particles(n::Int, x::U.Value) = MCM.Particles(n, MCM.Normal(U.value(x), U.uncertainty(x)))
+(::Type{PT})(n::Int, x::U.Value) where {PT <: MCM.AbstractParticles} = PT(n, MCM.Normal(U.value(x), U.uncertainty(x)))
 
 end
