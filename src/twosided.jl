8c18728dea2f1342d202a10d283340db026568de
Alexander Plavin <alexander@plav.in>
2025-03-01 00:00:00 +0000

    add check that TwoSided args are positive

--- src/twosided.jl
+++ src/twosided.jl
@@ -8,6 +8,20 @@ Uncertainty of `TwoSided(x, x)` should behave like just `x`. Only makes sense fo
 struct TwoSided{T}
     lo::T
     hi::T
+
+    function TwoSided(lo, hi)
+        lohi_p = promote(lo, hi)
+        return TwoSided{typeof(first(lohi_p))}(lohi_p...)
+    end
+
+    TwoSided{T}(lo, hi) where {T} = TwoSided{T}(convert(T, lo), convert(T, hi))
+
+    function TwoSided{T}(lo::T, hi::T) where {T}
+        if lo < zero(lo) || hi < zero(hi)
+            throw(ArgumentError("lo and hi should be positive, got $lo and $hi"))
+        end
+        return new{T}(lo, hi)
+    end
 end
 
 Base.show(io::IO, u::TwoSided) = print(io, "TwoSided(", u.lo, ", ", u.hi, ")")
@@ -16,6 +30,8 @@ Base.float(::Type{TwoSided{T}}) where {T} = TwoSided{float(T)}
 
 Base.:*(a::TwoSided, b) = @modify(p -> p * b, a[∗ₚ])
 Base.:*(a, b::TwoSided) = @modify(p -> a * p, b[∗ₚ])
+Base.:/(a::TwoSided, b) = @modify(p -> p / b, a[∗ₚ])
+Base.:/(a, b::TwoSided) = @modify(p -> a / p, b[∗ₚ])
 
 for f in [:rad2deg, :deg2rad]
     @eval Base.$f(a::TwoSided) = @modify($f, a[∗ₚ])
@@ -25,3 +41,13 @@ width(a::TwoSided) = a.lo + a.hi
 maxdiff(a::TwoSided) = max(a.lo, a.hi)
 
 Base.convert(::Type{T}, u::Number) where {T <: TwoSided} = T(u, u)
+
+
+_u_lo(u::Number) = u
+_u_lo(u::TwoSided) = u.lo
+_u_hi(u::Number) = u
+_u_hi(u::TwoSided) = u.hi
+_u_lohi(u, sign::Int) =
+    sign == +1 ? _u_hi(u) :
+    sign == -1 ? _u_lo(u) :
+    throw(ArgumentError("sign should be +1 or -1, got $sign"))
