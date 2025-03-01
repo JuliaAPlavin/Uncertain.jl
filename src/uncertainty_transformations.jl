c49abcba99b689e25f2a048e2080bae2ba565cae
Alexander Plavin <alexander@plav.in>
2025-03-01 00:00:00 +0000

    add UncertaintyTransformationF

--- src/uncertainty_transformations.jl
+++ src/uncertainty_transformations.jl
@@ -2,9 +2,17 @@ abstract type UncertaintyTransformation end
 
 (t::UncertaintyTransformation)(x::Value) = @modify(t, uncertainty(x))
 
+struct UncertaintyTransformationF{F} <: UncertaintyTransformation
+    f::F
+end
+
+(t::UncertaintyTransformationF)(u) = t.f(u)
+(t::UncertaintyTransformationF)(x::Value) = @modify(t, uncertainty(x))  # disambiguation
+
 struct LinearAdd{T} <: UncertaintyTransformation
     add::T
 end
 
 (t::LinearAdd{<:Number})(u::Number) = u + t.add
 (t::LinearAdd{<:Number})(u::TwoSided) = @modify(p -> p + t.add, u[∗ₚ])
+(t::LinearAdd{<:Number})(x::Value) = @modify(t, uncertainty(x))  # disambiguation
