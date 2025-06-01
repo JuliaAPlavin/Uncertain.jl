9a6abe980cd8db725148946a51b5fb5cf26dff78
Alexander Plavin <alexander@plav.in>
2025-06-01 00:00:00 +0000

    fix very slow weightedmean

--- src/agg.jl
+++ src/agg.jl
@@ -2,8 +2,8 @@ function weightedmean(xs)
     isempty(xs) && return convert(eltype(xs), Value(NaN, Inf)*oneunit(eltype(xs)))
     # XXX: using .+ instead of map() fails inference
     agg = mapreduce((a, b) -> map(+, a, b), xs) do x::Value
-        w = inv(uncertainty(x))^2
-        x * w, w
+        w = uncertainty(x)^(-2)
+        value(x) * w, w
     end
-    return agg[1] / agg[2]
+    return U.Value(agg[1] * inv(agg[2]), sqrt(inv(agg[2])))
 end
