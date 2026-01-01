e82254d1adeb028af7ed03127b22f09d811665e8
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    add TwoSided

---
+++ test/twosided.jl
@@ -0,0 +1,15 @@
+@testitem "one vs two sided" begin
+    using Unitful
+
+    @test U.width(0.1) == 0.2
+    @test U.maxdiff(0.1) == 0.1
+
+    @test U.width(U.TwoSided(0.1, 0.2)) ≈ 0.3
+    @test U.maxdiff(U.TwoSided(0.1, 0.2)) == 0.2
+    @test U.maxdiff(U.TwoSided(0.2, 0.1)) == 0.2
+
+    @test U.TwoSided(0.1, 0.2) * 2 == U.TwoSided(0.2, 0.4)
+    @test 2 * U.TwoSided(0.1, 0.2) == U.TwoSided(0.2, 0.4)
+    @test U.TwoSided(0.1, 0.2) * u"m" == U.TwoSided(0.1u"m", 0.2u"m")
+    @test 1u"m" * U.TwoSided(0.1, 0.2) == U.TwoSided(0.1u"m", 0.2u"m")
+end
