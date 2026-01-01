873379f2f2c5efa526453176b35fde0c11da0566
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    docstrings and readme

--- src/Uncertain.jl
+++ src/Uncertain.jl
@@ -10,6 +10,10 @@ include("maths.jl")
 include("agg.jl")
 include("disambiguation.jl")
 
+"""    ±ᵤ(val, unc)
+
+Alias for the `Value(val, unc)` constructor.
+"""
 ±ᵤ(v, e) = Value(v, e)
 
 Base.promote_rule(::Type{<:ValueNumber{TM,SM}}, ::Type{T}) where {T<:Number,TM,SM} = ValueNumber{promote_type(T, TM), promote_type(real(T), SM)}
@@ -21,7 +25,29 @@ Base.promote_rule(::Type{<:ValueReal{T1,S1}}, ::Type{<:ValueNumber{T2,S2}}) wher
 Base.promote_rule(::Type{<:ValueNumber{T2,S2}}, ::Type{<:ValueReal{T1,S1}}) where {T1,T2,S1,S2} = ValueNumber{promote_type(T1, T2), promote_type(S1, S2)}
 Base.promote_rule(::Type{<:ValueAny{T1,S1}}, ::Type{<:ValueAny{T2,S2}}) where {T1,T2,S1,S2} = ValueAny{promote_type(T1, T2), promote_type(S1, S2)}
 
+"""    nσ(x)
+
+How many uncertainties `x` is away from zero. For numeric values, this is `abs(U.value(x)) / U.uncertainty(x)`.
+"""
 nσ(x) = abs(U.value(x)) / U.uncertainty(x)
+
+"""    width(uncertainty)
+
+The width of an uncertainty object: e.g., `2u` for `u` being a number, or `lo + hi` for `TwoSided`.
+Only make sense for numbers with a natural order.
+"""
+function width end
+
+"""    maxdiff(uncertainty)
+
+Maximum difference from the nominal value allowed by an uncertainty object.
+Assumes the natural norm for the values, e.g., `abs` for `Number`.
+"""
+function maxdiff end
+
+width(a::Number) = 2a
+maxdiff(a::Number) = a
+
 
 baremodule U
 export Value, ValueAny, ValueNumber, ValueReal, value, uncertainty, nσ, weightedmean, ±
