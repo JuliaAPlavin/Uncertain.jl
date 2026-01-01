2c9c36f2f8bdbf71d88df9cfcf4be848df34b82c
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    fix weightedmean for unitful

--- src/agg.jl
+++ src/agg.jl
@@ -1,5 +1,5 @@
 function weightedmean(xs)
-    isempty(xs) && return convert(eltype(xs), Value(NaN, Inf))
+    isempty(xs) && return convert(eltype(xs), Value(NaN, Inf)*oneunit(eltype(xs)))
     # XXX: using .+ instead of map() fails inference
     agg = mapreduce((a, b) -> map(+, a, b), xs) do x::Value
         w = inv(uncertainty(x))^2
