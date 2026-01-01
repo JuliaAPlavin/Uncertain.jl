bb3c85520dda3d6c1141637a925c4f5af65c5af4
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    relax types in unitful

--- ext/UnitfulExt.jl
+++ ext/UnitfulExt.jl
@@ -13,15 +13,15 @@ Base.promote_rule(::Type{Quantity{S,D,U_}}, ::Type{<:U.ValueNumber{T,TE}}) where
 Base.promote_rule(::Type{Quantity{S,D,U_}}, ::Type{<:U.ValueReal{T,TE}}) where {S, D, U_, T, TE} =
     return U.ValueReal{promote_type(Quantity{S,D,U_}, T), promote_type(Quantity{S,D,U_}, TE)}
 
-function Base.:*(x::Union{U.ValueNumber,U.ValueReal}, y::Unitful.Units, z::Unitful.Units...)
+function Base.:*(x::U.Value, y::Unitful.Units, z::Unitful.Units...)
     u = *(y, z...)
     U.Value(U.value(x) * u, U.uncertainty(x) * u)
 end
 
 for f in [:*, :/]
-    @eval Base.$f(x::Union{U.ValueNumber,U.ValueReal}, y::Unitful.Quantity) = U.Value($f(U.value(x), y), $f(U.uncertainty(x), y))
-    @eval Base.$f(x::Union{U.ValueNumber,U.ValueReal}, y::Unitful.FreeUnits) = U.Value($f(U.value(x), y), $f(U.uncertainty(x), y))
-    @eval Base.$f(y::Unitful.Quantity, x::Union{U.ValueNumber,U.ValueReal}) = U.Value($f(y, U.value(x)), $f(y, U.uncertainty(x)))
+    @eval Base.$f(x::U.Value, y::Unitful.Quantity) = U.Value($f(U.value(x), y), $f(U.uncertainty(x), y))
+    @eval Base.$f(x::U.Value, y::Unitful.FreeUnits) = U.Value($f(U.value(x), y), $f(U.uncertainty(x), y))
+    @eval Base.$f(y::Unitful.Quantity, x::U.Value) = U.Value($f(y, U.value(x)), $f(y, U.uncertainty(x)))
 end
 
 end
