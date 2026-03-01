85eb76fe4e05facbdc0091f68b6c6b8805d74d64
Alexander Plavin <alexander@plav.in>
2026-03-01 00:00:00 +0000

    fix test

--- test/types.jl
+++ test/types.jl
@@ -136,8 +136,7 @@ end
         @test MCM.pmean(pv1[1]) ≈ 3 rtol=0.1
         @test MCM.pstd(pv1[1]) ≈ 0.3 rtol=0.2
         @test MCM.pstd(pv1[2]) ≈ 0.4 rtol=0.2
-        using Statistics: cor
-        @test cor(pv1[1].particles, pv1[2].particles) ≈ 1 atol=1e-10
+        @test pv1[1].particles ≈ pv1[2].particles rtol=1e-5
 
         # singular: σy=0
         v0 = SVector(3.0, 4.0) ±ᵤ U.CovMat(Symmetric(SMatrix{2,2}(0.09, 0.0, 0.0, 0.0)))
